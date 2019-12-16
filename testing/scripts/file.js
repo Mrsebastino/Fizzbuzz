577bc56347c71b51b91d5c0352fe34e46222bf7a
mrsebastino <mrsebastino@gmail.com>
2019-12-16 14:31:35 +0000

    testing fizzbuzz

---
+++ testing/scripts/file.js
@@ -0,0 +1,12 @@
+fizzBuzz = function (number) {
+    if (typeof (number) === "number") {
+        if (number % 3 === 0 && number % 5 === 0);
+        return "Fizzbuzz";
+    } else if (number % 3 === 0) {
+        return "Fizz";
+    } else if (number % 5 === 0) {
+        return "Buzz";
+    } else {
+        return number;
+    }
+};
